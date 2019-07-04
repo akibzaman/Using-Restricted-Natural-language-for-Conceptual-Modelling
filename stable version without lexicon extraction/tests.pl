138e031eddcca3b92349cef943b5b3d946e58a82
kushol <ashik1876@gmail.com>
2019-07-04 18:18:44 +1000

    Old stable implementation
    without lexicon extraction

---
+++ stable version without lexicon extraction/tests.pl
@@ -0,0 +1,16 @@
+specification(1,
+[
+['Every', student,is,an,entity,'.'],
+['Every','program',is,an,entity,'.'],
+['Every','unit',is,an,entity,'.'],
+['No',student,is,a,program,'.'],
+['No',student,is,a,unit,'.'],
+['No',unit,is,a,program,'.'],
+['Every',student,is,enrolled,in,exactly,1,program,'.'],
+['Every',program,is,composed,of,some,units,'.'],
+['Every',unit,is,studied,by,some,students,'.'],
+['Every',unit,is,belongs,to,some,programs,'.'],
+['Every',student,possesses,a,student,id,and,possesses,a,student,name,'.'],
+['Every',program,possesses,a,program,id,and,possesses,a,program,name,'.'],
+['Every',unit,possesses,a,unit,code,and,possesses,a,unit,name,'.'],
+['Every',student,studies,at,least,1,and,at,most,4,units,'.']]).
