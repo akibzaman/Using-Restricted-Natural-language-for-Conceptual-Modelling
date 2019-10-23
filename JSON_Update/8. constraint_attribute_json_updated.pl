835007261c5c181b53583a588bfbe352940719a8
Bayzid <ashik1876@gmail.com>
2019-10-23 12:03:06 +1100

    To be checked by Rolf

--- JSON_Update/8. constraint_attribute_json_updated.pl
+++ JSON_Update/8. constraint_attribute_json_updated.pl
@@ -1,10 +1,8 @@
 :- style_check([-discontiguous, -singleton]).
 :- use_module(library(http/json)).
   
-:- op(900, yfx, '==>').
 :- op(800, yfx, '&').
 :- op(900, yfx, ':').
-:- op(800, yfx, '#').  
 :- consult(predicates). 
 
 s([type:const_dp, sem:Sem]) -->
